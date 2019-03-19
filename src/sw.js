8b015c4b0610952e3f451ca5ce048abecbcb37a5
Rick Stanley <rick-stanley@hotmail.com>
2019-03-19 11:50:01 -0400

    Service worker cache update

--- src/sw.js
+++ src/sw.js
@@ -18,4 +18,28 @@ self.addEventListener('fetch', function (event) {
             return response || fetch(event.request);
         })
     );
+    event.waitUntil(update(event.request).then(refresh));
 });
+
+function update(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response.clone()).then(function () {
+                return response;
+            });
+        });
+    });
+}
+
+function refresh(response) {
+    return self.clients.matchAll().then(function (clients) {
+        clients.forEach(function (client) {
+            var message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
+            };
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
